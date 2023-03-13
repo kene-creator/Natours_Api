43a8295b6fb3039413d17ea680a9979922107b35
Kene <kayyungx@gmail.com>
2023-03-13 20:11:10 +0100

    connect db with mongoose

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 //SERVER
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connection established'));
 
 const app = require('./app');
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
-  console.log('App running on port: ' + port);
+  console.log(`App running on port: ${port}`);
 });
