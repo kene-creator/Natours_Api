11f886abdd81a2af2b841c1ee31f2e958a416ce5
Kene <kayyungx@gmail.com>
2023-03-14 08:11:07 +0100

    Add tour shema and model

--- server.js
+++ server.js
@@ -18,6 +18,34 @@ mongoose
 
 const app = require('./app');
 
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The forest hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => console.log('Error', err));
+
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`App running on port: ${port}`);
