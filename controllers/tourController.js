69d0a07a5cfa2f0ebaa0d57042684b571d4be6f7
Kene <kayyungx@gmail.com>
2023-03-15 13:34:42 +0100

    advance filter query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,13 +2,17 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    // Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
-    console.log(req.query, queryObj);
+
+    //Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`); //replace gte, gt, lte, lt with $gte, $gt, $lte, $lt
 
     //Build query
-    const query = Tour.find(queryObj);
+    const query = Tour.find(JSON.parse(queryStr));
 
     // const tours = await Tour.find()
     //   .where('duration')
