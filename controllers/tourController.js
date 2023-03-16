1bf40c7848a47b749b0ace3e185b0e49eee9f656
Kene <kayyungx@gmail.com>
2023-03-16 11:10:51 +0100

    Add api pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,6 +29,26 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    //FIELD LIMITING
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     //EXECUTE QUERY
     const tours = await query;
 
