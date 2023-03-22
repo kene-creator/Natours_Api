ba7791ccc32827a8d3a7d9c9f44fbde6d84bf204
Kene <kayyungx@gmail.com>
2023-03-22 16:50:42 +0100

    Add middleware to protect routes from unauthorized access

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,3 +50,20 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new AppError('You must login to go access to this page', 401));
+  }
+
+  console.log(token);
+  next();
+});
