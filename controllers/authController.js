0e089e30d9fbc8e404cd7aafe618004e8e9b5874
Kene <kayyungx@gmail.com>
2023-03-22 14:19:09 +0100

    Add login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,40 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  //   const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    next(new AppError('Please provide email and password', 400));
+  }
+
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
