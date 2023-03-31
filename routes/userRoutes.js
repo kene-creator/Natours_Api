3bf08e0c9e095f760cc08ed1c52469d576b6d057
Kene <kayyungx@gmail.com>
2023-03-31 11:41:07 +0100

    Add password update route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,9 @@ router.route('/login').post(authController.login);
 
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:token').patch(authController.resetPassword);
+router
+  .route('/updateMyPassword')
+  .patch(authController.protect, authController.updatePassword);
 
 router
   .route('/')
